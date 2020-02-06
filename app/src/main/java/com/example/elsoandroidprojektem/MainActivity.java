8606e0472d410c4926b96fc1c58670a371b96aa7
Devon <dobi4554@gmail.com>
2020-02-06 19:46:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/elsoandroidprojektem/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.elsoandroidprojektem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(this, "Elindítva", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(this, "Leállítva", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "Megsemmisítve", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(this, "Lepillanatályozva", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(this, "Folytatva", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(this, "Újraindítva", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
