a59134d78fa63b9679b1739514d7d69f91467a43
Devon <dobi4554@gmail.com>
2020-02-07 17:37:48 +0100

    Mukodo osszeadás

--- app/src/main/java/com/example/elsoandroidprojektem/MainActivity.java
+++ app/src/main/java/com/example/elsoandroidprojektem/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.elsoandroidprojektem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button kiszamolas;
+    EditText elsoSzam, masodikSzam;
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -44,9 +51,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kiszamolas =  findViewById(R.id.button);
+        elsoSzam = findViewById(R.id.szam);
+        masodikSzam = findViewById(R.id.szam2);
+
+        kiszamolas.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                int eredmeny = Integer.parseInt(elsoSzam.getText().toString()) + Integer.parseInt(masodikSzam.getText().toString());
+                Toast.makeText(this, "Az összeadás eredménye: " + String.valueOf(eredmeny), Toast.LENGTH_LONG).show();
+                break;
+        }
+
+
     }
 }
